c4b5d6d0b03f1cbf92e5f5f815eb22ef625ab1d0
Henry Alexander Mariño Avila <henrymarinho90@gmail.com>
2022-11-04 21:40:47 -0500

    Bootstrap added
    git add --all

---
+++ AMRAsociadosWeb/src/app/Components/carousel/carousel.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({ selector: 'ngb-carousel', templateUrl:'./carousel.component.html' })
+export class NgbdCarouselBasic {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
--- AMRAsociadosWeb/src/app/app.module.ts
+++ AMRAsociadosWeb/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './Components/topbar/topbar.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { NgbdCarouselBasic } from './Components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent
+    TopbarComponent,
+    NavbarComponent,
+    NgbdCarouselBasic
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
